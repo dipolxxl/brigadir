e24552bd42c8bb4355b601a70e2ce82226102420
Dmitriy Polozov <dipol.xxl@gmail.com>
2013-06-05 22:10:23 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brigadir::Application.config.secret_token = '0054d82102d90b8ae9e10f8428887b53f47c2359555f705ccb9be4a1ef34d6a9627d1eb69b7e13f727d2ce7dbefa5e2cce4295d6b003b4f5b507fc9354a5540a'
